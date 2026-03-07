27db50dea9abac0c4fa1373edda7a264e67210ec
Niranjan070 <nirthangam6474@gmail.com>
2026-03-07 19:09:26 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,16 @@ export default function App() {
       <svg style={{ position: 'absolute', width: 0, height: 0 }}>
         <filter
           id="glass-distortion"
-          x="0%" y="0%" width="100%" height="100%"
+          x="-5%" y="-5%" width="110%" height="110%"
           filterUnits="objectBoundingBox"
         >
-          <feTurbulence type="fractalNoise" baseFrequency="0.001 0.005" numOctaves="1" seed="17" result="turbulence" />
-          <feComponentTransfer in="turbulence" result="mapped">
-            <feFuncR type="gamma" amplitude="1" exponent="10" offset="0.5" />
-            <feFuncG type="gamma" amplitude="0" exponent="1" offset="0" />
-            <feFuncB type="gamma" amplitude="0" exponent="1" offset="0.5" />
-          </feComponentTransfer>
-          <feGaussianBlur in="turbulence" stdDeviation="3" result="softMap" />
-          <feSpecularLighting in="softMap" surfaceScale="5" specularConstant="1" specularExponent="100" lightingColor="white" result="specLight">
+          <feTurbulence type="fractalNoise" baseFrequency="0.015 0.02" numOctaves="2" seed="17" result="turbulence" />
+          <feGaussianBlur in="turbulence" stdDeviation="4" result="softMap" />
+          <feSpecularLighting in="softMap" surfaceScale="2" specularConstant="0.8" specularExponent="80" lightingColor="white" result="specLight">
             <fePointLight x="-200" y="-200" z="300" />
           </feSpecularLighting>
           <feComposite in="specLight" operator="arithmetic" k1="0" k2="1" k3="1" k4="0" result="litImage" />
-          <feDisplacementMap in="SourceGraphic" in2="softMap" scale="200" xChannelSelector="R" yChannelSelector="G" />
+          <feDisplacementMap in="SourceGraphic" in2="softMap" scale="30" xChannelSelector="R" yChannelSelector="G" />
         </filter>
       </svg>
 
